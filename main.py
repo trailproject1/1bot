550c4cfbdb8669655e68f299c13ad63aab9e85f2
trailproject1 <pythonwork786@gmail.com>
2026-08-04 22:17:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from telegram import Update
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    ContextTypes,
-)
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+from telegram.ext import Application, CommandHandler, ContextTypes
+from config import BOT_TOKEN
+from database import init_db
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "✅ Clear7 Bot is Online!"
+        "✅ Clear7_bot is Online!"
     )
 
 
@@ -26,12 +21,15 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 def main():
+    init_db()
+
     app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
 
-    print("✅ Clear7 Bot Started")
+    print("✅ Database Loaded")
+    print("✅ Clear7_bot Started")
 
     app.run_polling()
 
